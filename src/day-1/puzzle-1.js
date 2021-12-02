cd540baa0c1dcb4da996121e59a7ce2583ca1a13
Brad <bradtaniguchi@gmail.com>
2021-12-02 01:08:24 +0000

    Finalize first puzzle

--- src/day-1/puzzle-1.js
+++ src/day-1/puzzle-1.js
@@ -5,14 +5,29 @@ const { join } = require("path");
  * Challenge link:
  * https://adventofcode.com/2021/day/1
  */
-const puzzle1 = () => getContents();
+const puzzle1 = () => getContents().then(getIncreasing);
 
+/**
+ * Returns the raw contents of a file as an array of strings
+ */
 const getContents = () =>
   readFile(join(__dirname, "./puzzle-1-input.txt"), "utf-8").then((contents) =>
-    contents.split("\n")
+    contents.split("\n").map(Number)
+  );
+
+/**
+ * Returns the count of increasing numbers
+ * @param num {number[]} array of numbers to get the number of
+ * increasing numbers
+ */
+const getIncreasing = (nums) =>
+  nums.reduce(
+    (acc, num, index, arr) => (arr[index - 1] < num ? (acc += 1) : acc),
+    0
   );
 
 module.exports = {
   puzzle1,
-  getContents
+  getContents,
+  getIncreasing,
 };
--- src/day-1/puzzle-1.test.js
+++ src/day-1/puzzle-1.test.js
@@ -1,16 +1,38 @@
-const { puzzle1, getContents } = require("./puzzle-1");
+const { puzzle1, getContents, getIncreasing } = require("./puzzle-1");
 
 describe("puzzle-1", () => {
-  test("is function", () =>
-    expect(typeof getContents === "function").toBe(true));
+  test("is function", () => expect(typeof puzzle1 === "function").toBe(true));
+  test("returns challenge answer for input", () =>
+    expect(puzzle1()).resolves.toEqual(1448));
 
   describe("getContents", () => {
     test("is function", () =>
       expect(typeof getContents === "function").toBe(true));
-    test("resolves array of strings", () =>
+    test("resolves array of numbers", () =>
       getContents().then((contents) => {
         expect(Array.isArray(contents)).toBe(true);
-        expect(typeof contents[0] === "string").toBe(true);
+        expect(typeof contents[0] === "number").toBe(true);
       }));
+  });
+
+  describe("getIncreasing", () => {
+    test("is function", () =>
+      expect(typeof getIncreasing === "function").toBe(true));
+    test("returns 0 if given empty array", () =>
+      expect(getIncreasing([])).toEqual(0));
+    test("returns 0 if given 1 item", () =>
+      expect(getIncreasing([199])).toEqual(0));
+    test("return 1 if given 2 items, increasing", () =>
+      expect(getIncreasing([199, 200])).toEqual(1));
+    test("return 0 if given 2 items, decreasing", () =>
+      expect(getIncreasing([200, 199, 198])).toEqual(0));
+    test("return 2 if given 3 items, increasing", () =>
+      expect(getIncreasing([199, 200, 205])).toEqual(2));
+    test("return 1 if given 3 items, mixed", () =>
+      expect(getIncreasing([199, 198, 200])).toEqual(1));
+    test("handles example case", () =>
+      expect(
+        getIncreasing([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+      ).toEqual(7));
   });
 });
