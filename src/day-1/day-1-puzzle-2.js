32f09344d3d82465f377fae4586cbc53a3e61679
Brad <bradtaniguchi@gmail.com>
2021-12-02 05:45:14 +0000

    fix issue with day-1-puzzle-2

--- src/day-1/day-1-puzzle-2.js
+++ src/day-1/day-1-puzzle-2.js
@@ -1,7 +1,11 @@
 const { readFile } = require("fs").promises;
 const { join } = require("path");
 
-const day1Puzzle2 = () => {};
+/**
+ * Puzzle 2 for day 1:
+ * https://adventofcode.com/2021/day/1#part2
+ */
+const day1Puzzle2 = () => getContents().then(getGroupIncreasing);
 
 /**
  * Returns the raw contents of a file as an array of strings
@@ -11,7 +15,29 @@ const getContents = () =>
     (contents) => contents.split("\n").map(Number)
   );
 
+/**
+ * Returns the grouped increasing amount.
+ * @params nums {number[]} array of numbers to group and check
+ * for increasing
+ */
+const getGroupIncreasing = (nums) => {
+  let groupsIncreasing = 0;
+  for (let index = 0; index < nums.length; index++) {
+    const first = nums[index];
+    const second = nums[index + 1];
+    const third = nums[index + 2];
+    const fourth = nums[index + 3];
+
+    const firstGroup = first + second + third;
+    const secondGroup = second + third + fourth;
+    if (firstGroup < secondGroup) groupsIncreasing++;
+  }
+
+  return groupsIncreasing;
+};
+
 module.exports = {
   day1Puzzle2,
   getContents,
+  getGroupIncreasing,
 };
--- src/day-1/day-1-puzzle-2.test.js
+++ src/day-1/day-1-puzzle-2.test.js
@@ -1,8 +1,14 @@
-const { day1Puzzle2, getContents } = require("./day-1-puzzle-2");
+const {
+  day1Puzzle2,
+  getContents,
+  getGroupIncreasing,
+} = require("./day-1-puzzle-2");
 
 describe("day1Puzzle2", () => {
   test("is function", () =>
     expect(typeof day1Puzzle2 === "function").toBe(true));
+  test("returns challenge answer for input", () =>
+    expect(day1Puzzle2()).resolves.toEqual(1471));
 
   describe("getContents", () => {
     test("is function", () =>
@@ -12,5 +18,26 @@ describe("day1Puzzle2", () => {
         expect(Array.isArray(contents)).toBe(true);
         expect(typeof contents[0] === "number").toBe(true);
       }));
+  });
+
+  describe("getGroupIncreasing", () => {
+    test("is function", () =>
+      expect(typeof getGroupIncreasing).toEqual("function"));
+    test("returns 0 if given empty array", () =>
+      expect(getGroupIncreasing([])).toEqual(0));
+    test("returns 0 if given 1 item", () =>
+      expect(getGroupIncreasing([199])).toEqual(0));
+    test("return 0 if given 2 items", () =>
+      expect(getGroupIncreasing([199, 200])).toEqual(0));
+    test("return 0 if given 3 items", () =>
+      expect(getGroupIncreasing([199, 200, 201])).toEqual(0));
+    test("return 1 if given 4 items, with second set increasing", () =>
+      expect(getGroupIncreasing([200, 200, 200, 201])).toEqual(1));
+    test("return 0 if given 4 items, with second set decreasing", () =>
+      expect(getGroupIncreasing([200, 200, 200, 199])).toEqual(0));
+    test("handles example case", () =>
+      expect(
+        getGroupIncreasing([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+      ).toEqual(5));
   });
 });
