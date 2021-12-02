4ebab4fee68d5f2d75527b6e2d97dbae789378bf
Brad <bradtaniguchi@gmail.com>
2021-12-02 00:48:29 +0000

    Update test and code structure to support tdd

--- src/day-1/puzzle-1.js
+++ src/day-1/puzzle-1.js
@@ -1,5 +1,18 @@
 const { readFile } = require("fs").promises;
 const { join } = require("path");
 
-// TODO: implement actual logic;
-module.exports = async () => readFile(join(__dirname, './puzzle-1-input.txt'), "utf-8");
+/**
+ * Challenge link:
+ * https://adventofcode.com/2021/day/1
+ */
+const puzzle1 = () => getContents().then(split);
+
+const getContents = () =>
+  readFile(join(__dirname, "./puzzle-1-input.txt"), "utf-8").then((contents) =>
+    contents.split("\n")
+  );
+
+module.exports = {
+  puzzle1,
+  getContents
+};
--- src/day-1/puzzle-1.test.js
+++ src/day-1/puzzle-1.test.js
@@ -1,8 +1,16 @@
-const puzzle1 = require("./puzzle-1");
+const { puzzle1, getContents } = require("./puzzle-1");
 
 describe("puzzle-1", () => {
-  test("is function", () => expect(typeof puzzle1 === "function").toBe(true));
-  // TODO: return actual value
-  test("returns promise of string", () =>
-    puzzle1().then((inputStr) => typeof inputStr === "string"));
+  test("is function", () =>
+    expect(typeof getContents === "function").toBe(true));
+
+  describe("getContents", () => {
+    test("is function", () =>
+      expect(typeof getContents === "function").toBe(true));
+    test("resolves array of strings", () =>
+      getContents().then((contents) => {
+        expect(Array.isArray(contents)).toBe(true);
+        expect(typeof contents[0] === "string").toBe(true);
+      }));
+  });
 });
